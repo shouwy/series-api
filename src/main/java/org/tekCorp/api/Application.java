878b2b71710f72a799e54464023a555598b8e448
shouwy <thierry.freres1@gmail.com>
2018-12-04 11:00:31 +0100

    Fix SpringBootServletInitializer

--- src/main/java/org/tekCorp/api/Application.java
+++ src/main/java/org/tekCorp/api/Application.java
@@ -3,7 +3,7 @@ package org.tekCorp.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Created by Inspiron on 15/06/2015.
