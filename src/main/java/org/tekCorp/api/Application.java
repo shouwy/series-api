4158617efae929d16058fa3a4a1ba2fc0239508f
shouwy <thierry.freres1@gmail.com>
2019-04-23 14:11:31 +0200

    ADD Travis CI

--- src/main/java/org/tekCorp/api/Application.java
+++ src/main/java/org/tekCorp/api/Application.java
@@ -2,20 +2,19 @@ package org.tekCorp.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Created by Inspiron on 15/06/2015.
  */
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-    @Override
-    public SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
-        return applicationBuilder.sources(Application.class);
-    }
-
+    /**
+     * Classe Main starter de l'application SpringBoot
+     *
+     * @param args argument de lancement
+     * @throws Exception Exception
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
